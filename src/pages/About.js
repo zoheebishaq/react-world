739fc08491bd0f9d3b9366e02d59fba239ac0582
zoheebishaq <zoheeb.ishaque@gmail.com>
2021-03-28 21:42:54 +0200

    v3

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,29 +5,48 @@ import Navigation from "../components/Navigation";
 const About = () => {
   return (
     <div>
-        <Navigation/>
-        <Logo/>
+      <Navigation />
+      <Logo />
       <h1>A propos</h1>
       <br />
       <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita
-        voluptatibus quaerat, nemo sunt cumque cum accusamus praesentium
-        consequatur sint reiciendis dolor? Optio consectetur alias aut eveniet
-        ducimus corrupti aspernatur dolor repellat corporis repellendus quas sit
-        reprehenderit totam laboriosam deleniti soluta aliquam rerum, porro
-        perferendis. Minus tempora exercitationem accusamus libero velit
-        adipisci rerum, voluptatem cupiditate aliquam, excepturi, consequuntur
-        omnis odit aperiam?
+        De nombreuses publications donnent une liste des pays du monde, mais
+        elles n'utilisent pas toutes les mêmes critères pour inclure ou non un
+        pays. Un critère courant mais flou est la reconnaissance du pays par la
+        communauté internationale, notamment sa reconnaissance par
+        l'Organisation des Nations unies qui compte 193 États membres et
+        reconnaît quatre États non membres (dont deux avec le statut
+        d’observateurs permanents).
       </p>
       <br />
       <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus cum
-        sequi dolores a asperiores ut velit consequatur nisi neque iusto quo
-        mollitia voluptate nesciunt id, error, minima magni doloremque aut eius
-        beatae illo? Perferendis molestiae magni unde quod assumenda explicabo,
-        rem itaque? Deserunt, voluptatibus ab! Quod voluptas nobis repudiandae
-        vel magni rem assumenda suscipit, corporis quia obcaecati, officiis
-        soluta nostrum!
+        Ces quatre États non membres sont le Vatican (observateur permanent
+        depuis 1964), l'État de Palestine (observateur permanent depuis 2012),
+        et les Îles Cook et Niue, deux territoires insulaires en libre
+        association avec la Nouvelle-Zélande.
+      </p>
+      <br />
+      <p>
+        Six pays sont reconnus par au moins un État membre de l'ONU sans être
+        reconnus par l'ONU : le Kosovo, la République arabe sahraouie
+        démocratique, la république de Chine (Taïwan), l'Abkhazie, l'Ossétie du
+        Sud, la république turque de Chypre du Nord. En outre, la république
+        moldave de Transnistrie et la république du Haut-Karabagh se
+        reconnaissent mutuellement, et sont reconnues par l'Ossétie du Sud et
+        l'Abkhazie. La crise ukrainienne a donné lieu à l'apparition de deux
+        entités non reconnues, la république populaire de Donetsk et la
+        république populaire de Lougansk. Enfin, un territoire indépendant de
+        facto depuis 1990 n'est également reconnu par aucun pays, la république
+        du Somaliland.
+      </p>
+      <br />
+      <p>
+        Lorsque Taïwan est inclus dans une liste, c'est souvent accompagné d'un
+        avertissement sur le caractère non officiel de la listenote ou sur le
+        fait que Taïwan n'est pas reconnunote. En effet, le statut de Taïwan est
+        particulier du fait que la république populaire de Chine exige la
+        reconnaissance du principe d'une seule Chine de la part des pays qui
+        veulent avoir des rapports diplomatiques avec elle.
       </p>
     </div>
   );
